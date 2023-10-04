de86223e647a3ceb67412fd1e62dcc6b0d82110a
zagumennikov.yy1229 <zagumennikov.yy1229@COM407-L1>
2023-10-04 09:14:54 +0300

    "любимая" практика

---
+++ Практическая работа №4. Эмуляция критической секции/DataBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class DataBase
+{
+    public void SaveData(string text)
+    {
+        lock (this)
+        {
+            Console.WriteLine("Database. SaveData - Started");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("x");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine();
+            Console.WriteLine("dataBase. SaveData - working");
+            Thread.Sleep(2000);
+            Console.WriteLine("dataBase. SaveData - ended");
+        }
+    }
+}
---
+++ Практическая работа №4. Эмуляция критической секции/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Threading;
+
+namespace MultiThreadingExample
+{
+    class Program
+    {
+        public static DataBase db = new DataBase();
+
+        static void WorkerThreadMethod1()
+        {
+            Console.WriteLine("worker thread #1 - started");
+            db.SaveData("worker thread #1 - Calling Database. Savedata");
+            Console.WriteLine("worker thread #1 - Returned from output");
+        }
+
+        static void WorkerThreadMethod2()
+        {
+            Console.WriteLine("worker thread #2 - started");
+            db.SaveData("worker thread #2 - Calling Database. Savedata");
+            Console.WriteLine("worker thread #2 - Returned from output");
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadStart worker1 = new ThreadStart(WorkerThreadMethod1);
+            ThreadStart worker2 = new ThreadStart(WorkerThreadMethod2);
+
+            Thread t1 = new Thread(worker1);
+            Thread t2 = new Thread(worker2);
+
+            t1.Start();
+            t2.Start();
+
+            Console.WriteLine("main - creating worker threads");
+
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine("main - all done");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
